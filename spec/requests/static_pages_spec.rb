514076cf39289c1d5083e5584cf3cd9c3f3621b4
Kyrylo Shegeda <shegeda@ualberta.ca>
2013-08-09 13:06:36 -0600

    Separate the common part of a title in static_pages test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-
+  let (:base_title) {"Ruby on Rails Tutorial"}
   describe "Home Page" do
 
     it "should have content 'Sample App'" do
@@ -12,7 +12,7 @@ describe "StaticPages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -24,7 +24,7 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -36,7 +36,7 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title('About Us')
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
@@ -46,9 +46,9 @@ describe "StaticPages" do
       expect(page).to have_content('Contact')
     end
 
-    it "should have title 'Ruby on Rails Tutorial Sample App | Contact'" do
+    it "should have title 'Ruby on Rails Tutorial | Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title('Ruby on Rails Tutorial | Contact')
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
