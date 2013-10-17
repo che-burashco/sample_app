747374cc3460753be70b90a8214ae9f3381a0753
Kyrylo Shegeda <shegeda@ualberta.ca>
2013-10-17 18:37:54 -0400

    Erase old code

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,13 +1,1 @@
 include ApplicationHelper
-
-=begin
-def full_title(page_title)
-	base_title = "Ruby on Rails Tutorial"
-	if (page_title.empty?)
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-
-end
-=end
