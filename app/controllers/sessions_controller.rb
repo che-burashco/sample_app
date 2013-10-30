8d5e025c1abf96b3d19cc40d751bb9b56f76d13e
Kyrylo Shegeda <shegeda@ualberta.ca>
2013-10-30 08:30:17 -0400

    Handle failed sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,15 @@ class SessionsController < ApplicationController
 	def new
 	end
 
+	def create
+		user = User.find_by(email: params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+		else
+			flash.now[:error] = "Invalid email/password combination"
+			render 'new'
+		end
+	end
+
 	def edit
 	end
 
